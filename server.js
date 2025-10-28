88facb11e05125e1a0d19a43f1186d94fdc2f1ac
Bayuajinugroho16 <aji13413@gmail.com>
2025-10-29 03:53:32 +0700

    bismillah

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
 import { pool } from './config/database.js';
 import multer from 'multer';
+import authRoutes from './routes/auth.js';
 import { createClient } from '@supabase/supabase-js';
 import path from 'path';
 import cron from 'node-cron';
@@ -33,6 +34,7 @@ app.options('*', cors()); // handle preflight for all routes
 // ✅ INCREASE PAYLOAD LIMIT
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ extended: true, limit: '50mb' }));
+app.use('/api/auth', authRoutes);
 
 // ==================== SUPABASE CONFIG ====================
 const supabase = createClient(
